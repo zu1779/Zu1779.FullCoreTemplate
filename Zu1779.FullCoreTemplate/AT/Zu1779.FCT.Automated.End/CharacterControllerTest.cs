6762f939575ad63495669d0b3edd54668283fdef
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2019-05-23 18:32:37 +0200

    .

--- Zu1779.FullCoreTemplate/AT/Zu1779.FCT.Automated.End/CharacterControllerTest.cs
+++ Zu1779.FullCoreTemplate/AT/Zu1779.FCT.Automated.End/CharacterControllerTest.cs
@@ -7,16 +7,28 @@
 
     using Zu1779.FCT.Client.RPGClient;
 
-    public class CharacterTest
+    public class CharacterControllerTest
     {
         [Fact]
-        public async Task Random_generation_success()
+        public async Task Random_abilities_generation_success()
         {
             using (var httpClient = new HttpClient())
             {
                 var client = new Client("http://localhost:53085/", httpClient);
                 var response = await client.RandomAbilitiesAsync(null);
                 Assert.Equal(6, response.Count);
+                Assert.All(response, c => Assert.InRange(c, 3, 18));
+            }
+        }
+
+        [Fact]
+        public async Task Random_ability_generation_success()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var client = new Client("http://localhost:53085/", httpClient);
+                var response = await client.RandomAbilityAsync();
+                Assert.InRange(response, 3, 18);
             }
         }
     }
