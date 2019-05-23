cbfc4102e1d4ecb72590aec0c643b204fbb3595c
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2019-05-23 17:25:37 +0200

    .

---
+++ Zu1779.FullCoreTemplate/AT/Zu1779.FCT.Automated.End/CharacterControllerTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Zu1779.FCT.Automated.End
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Xunit;
+
+    using Zu1779.FCT.Client.RPGClient;
+
+    public class CharacterTest
+    {
+        [Fact]
+        public async Task Random_generation_success()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var client = new Client("http://localhost:53085/", httpClient);
+                var response = await client.RandomAbilitiesAsync(null);
+                Assert.Equal(6, response.Count);
+            }
+        }
+    }
+}
