cb9c136aeafde46e6edc5dd4fdfa12a5d9af31e0
Mauro Dalla Zuanna <maurodallazuanna@hotmail.com>
2019-05-25 16:19:40 +0200

    .

--- Zu1779.FullCoreTemplate/Presentation/Zu1779.FCT.Presentation.RPGWebMVC/Controllers/CharacterController.cs
+++ Zu1779.FullCoreTemplate/Presentation/Zu1779.FCT.Presentation.RPGWebMVC/Controllers/CharacterController.cs
@@ -8,6 +8,7 @@
     using Microsoft.AspNetCore.Mvc;
 
     using Zu1779.FCT.Client.RPGClient;
+    using cm = Zu1779.FCT.Contract.RPGContract.Models;
     using m = Zu1779.FCT.Presentation.RPGWebMVC.Models;
 
     public class CharacterController : Controller
@@ -28,6 +29,24 @@
             var randomNumber = await rpgClient.RandomAbilitiesAsync(null);
             if (model.RandomNumber == null) model.RandomNumber = new Queue<byte>(randomNumber.Select(c => Convert.ToByte(c)));
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SetAbility([FromHeader]m.CharacterModel model, cm.AbilityEnum ability)
+        {
+            var abilityProperty = model.GetType().GetProperty(ability.ToString());
+            var abilityValue = (byte)abilityProperty.GetValue(model);
+            if (abilityValue != 0)
+            {
+                model.RandomNumber.Enqueue(abilityValue);
+                abilityProperty.SetValue(model, 0);
+            }
+            else if (model.RandomNumber.Count > 0)
+            {
+                var value = model.RandomNumber.Dequeue();
+                abilityProperty.SetValue(model, value);
+            }
+            return View("Edit", model);
         }
     }
 }
